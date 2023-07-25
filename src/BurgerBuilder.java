4155a50355cb24aa767afe92c76eca6440e67901
cosmoclast <bernardyeobk@hotmail.com>
2023-07-26 00:15:45 +0800

    Working Copy of Burger Builder!

---
+++ src/BurgerBuilder.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class BurgerBuilder {
+
+    private Stack<String> burger;
+
+    public BurgerBuilder(){
+        this.burger = new Stack<>();
+    }
+    public BurgerBuilder addBun(){
+        this.burger.push("Bun");
+        return this;
+    }
+    public BurgerBuilder addPatty(){
+        this.burger.push("Patty");
+        return this;
+    }
+    public BurgerBuilder addLettuce(){
+        this.burger.push("Lettuce");
+        return this;
+    }
+    public BurgerBuilder addSauce(){
+        this.burger.push("Sauce");
+        return this;
+    }
+    public BurgerBuilder addTomato(){
+        this.burger.push("Tomato");
+        return this;
+    }
+    public BurgerBuilder addCheese(){
+        this.burger.push("Cheese");
+        return this;
+    }
+    public void buildBurger(){
+        while(!burger.isEmpty()){
+            System.out.println(burger.pop());
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BurgerBuilder bb1 = new BurgerBuilder();
+        bb1.addBun().addLettuce().addSauce().addCheese().addPatty().addBun();
+        bb1.buildBurger();
     }
 }
